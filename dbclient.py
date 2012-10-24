e962b3b87fca2e9f290276e9346c613455576bca
Andy Goetz <andy@andygoetz.org>
2012-10-23 23:40:21 -0700

    Added database client

---
+++ dbclient.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python2
+
+import sqlite3
+import sys
+
+
+
+def print_help():
+    print '''
+dbclient.py Usage:
+
+dbclient.py [database] lx - List All Chromosomes
+dbclient.py [database] le - List All Experiments
+dbclient.py [database] lxe [eid] - List All Chromosomes from experiment eid
+dbclient.py [database] ae [name] [heuristics] - add experimment
+dbclient.py [database] ax [eid] [alive] [xc] [parents] [age] [score] add chromosome
+'''
+
+
+# dump all of the chromosome in the table to std out
+def list_xc(conn):
+    # get 'cursor'
+    c = conn.cursor()
+    for row in c.execute('SELECT * FROM chromosome'):
+        print row
+  
+# list all chromosomes that match a specific experiment
+def list_by_exp(conn, eid):
+    #get 'cursor'
+    c = conn.cursor()
+    for row in c.execute('SELECT * FROM chromosome WHERE eid = %s' % eid):
+        print row
+
+# list all of the experiments in the database
+def list_exp(conn):
+    # get 'cursor'
+    c = conn.cursor()
+    for row in c.execute('SELECT * FROM experiment'):
+        print row
+
+# add an experiment to the db
+def add_exp(conn, name, heuristics):
+    #get 'cursor'
+    c = conn.cursor()
+    c.execute('INSERT INTO experiment (name, heuristics) VALUES ("%s", "%s")' % (name, heuristics))
+
+def add_xc(conn, eid, alive, xc, parents, age, score):
+    # get 'cursor'
+    c = conn.cursor()
+    c.execute(('INSERT INTO chromosome (eid, alive, xc, parents, age, score) VALUES' + 
+              '("%s", %d, "%s", "%s", %d, %f)' % 
+               (eid, int(alive), xc, parents, int(age), float(score))))
+
+
+####START OF SCRIPT
+
+
+#Get the name of the database file to use
+if len(sys.argv) < 2:
+    print_help() 
+    sys.exit(-1)
+
+dbname = sys.argv[1]
+
+#make a connection to the database
+conn = sqlite3.connect(dbname)
+
+#once we have a connection we need a 'cursor' to actually do anything..
+c = conn.cursor()
+
+
+# first of all, we need to capture the schema available for the
+# database. There are two main tables:
+#
+# experiment: contains metadata about the experiment being executed
+# +------------------------+-----------+-----------------+
+# | id INTEGER PRIMARY KEY | name TEXT | heuristics BLOB |
+# +------------------------+-----------+-----------------+
+
+c.execute('''CREATE TABLE IF NOT EXISTS experiment 
+(id INTEGER PRIMARY KEY,
+name TEXT NOT NULL,
+heuristics BLOB NOT NULL)''')
+
+
+
+# chromosomes: table about each chromosome
+# +------------------------+----------------------+---------------+---------+--------------+-------------+------------+
+# | id INTEGER PRIMARY KEY | eid INTEGER NOT NULL | alive BOOLEAN | xc BLOB | parents BLOB | age INTEGER | score REAL |
+# +------------------------+- --------------------+---------------+---------+--------------+-------------+------------+
+c.execute('''CREATE TABLE IF NOT EXISTS chromosome
+(id INTEGER PRIMARY KEY,
+eid INTEGER NOT NULL,
+alive BOOLEAN NOT NULL,
+xc BLOB NOT NULL,
+parents BLOB,
+age INTEGER NOT NULL,
+score REAL NOT NULL,
+FOREIGN KEY(eid) REFERENCES experiment(id))''')
+
+
+if len(sys.argv) > 2:
+    if(sys.argv[2] == "lx"):
+        list_xc(conn)
+    elif(sys.argv[2] == "lxe" and len(sys.argv) >= 4):
+        list_by_exp(conn, sys.argv[3])
+    elif(sys.argv[2] == "ae" and len(sys.argv) >= 5):
+        add_exp(conn, sys.argv[3], sys.argv[4])
+    elif(sys.argv[2] == "le"):
+        list_exp(conn)
+    elif(sys.argv[2] == "ax" and len(sys.argv) >= 9):
+        add_xc(conn, sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8])
+    
+
+
+
+# close the db:
+conn.commit()
+conn.close()
+
+
+
+
+
+
+

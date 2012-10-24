841590ab756de742120f372d549a2859c12dc3f0
Camille Huffman Day <zombie.grl@gmail.com>
2012-10-23 21:04:21 -0700

    Hey this is the correct render.py module

--- render.py
+++ render.py
@@ -1,6 +1,87 @@
 import random
-import math
+from math import *
 from Tkinter import *
+
+DEF_WIDTH = 2
+
+def nextState(state):
+	random.seed()
+	weight = random.randint(0,4)
+	stateArray = ('L', 'A', 'T', 'T', 'T')
+	return stateArray[weight]
+	
+def boundaryAdjust(positionA, positionB, expectedA, expectedB, boundary):
+	return (expectedB - ((abs(expectedA - boundary)*(expectedB - positionB))/(expectedA - positionA)))
+
+def drawArc(x, y, vector, theta, r, cv):
+	getStartAngle = (-90, 0, 90, 180)
+	startAng = getStartAngle[random.randint(0,3)]
+	
+	if vector >= 0 & vector <= 45: startAng = 0
+	elif vector > 45 & vector <= 90: startAng = 90
+	elif vector > 90 & vector <= 180: startAng = 180
+	elif vector > 180 & vector < 360: startAng = -90
+	
+	bbox = x, y, x, y
+	xEnd = x 
+	yEnd = y
+	if (startAng == 0):
+		# Movement A, E
+		bbox = (x - 2*r), (y - r), (x), (y + r)
+		adjTheta = theta
+		xEnd = x - r*(1 - cos(radians(adjTheta)))
+
+		yEnd = y - r*sin(radians(adjTheta))
+	elif (startAng == 180):
+		# Movement C, G
+		bbox = (x), (y - r), (x + 2*r), (y + r)
+		adjTheta = theta
+		xEnd = x + r*(1 - cos(radians(adjTheta)))
+		yEnd = y + r*(sin(radians(adjTheta))) 
+	elif (startAng == 90):
+		# Movement F, H
+		bbox = (x - r), (y), (x + r), (y + 2*r)
+		adjTheta = startAng + theta
+		xEnd = x + r*cos(radians(adjTheta))
+		yEnd = y + r*(1 - sin(radians(adjTheta)))
+	elif (startAng == -90):
+		# Movement B, D
+		bbox = (x - r), (y - 2*r), (x + r), (y)
+		adjTheta = (270 + theta)
+		xEnd = x + r*(cos(radians(adjTheta)))
+		yEnd = y - r*(1 + sin(radians(adjTheta)))	
+	
+	if xEnd > 500:
+		yEnd = boundaryAdjust(x, y, xEnd, yEnd, 500)
+		xEnd = 500
+	if xEnd < 0: 
+		yEnd = boundaryAdjust(x, y, xEnd, yEnd, 0)
+		xEnd = 0
+	if yEnd > 500: 
+		xEnd = boundaryAdjust(y, x, yEnd, xEnd, 500)
+		yEnd = 500
+	if yEnd < 0: 
+		xEnd = boundaryAdjust(y, x, yEnd, xEnd, 0)
+		yEnd = 0
+	
+	cv.create_arc( bbox, width=DEF_WIDTH, start=startAng, extent=theta, style = ARC)
+	
+	return xEnd, yEnd
+
+def drawLine(x, y, vector, d, cv):
+
+	xEnd = x + (d * cos(radians(vector)))
+	yEnd = y + (d * sin(radians(vector)))
+	
+	if xEnd > 500: xEnd = 500
+	if xEnd < 0: xEnd = 0
+		
+	if yEnd > 500: yEnd = 500
+	if yEnd < 0: yEnd = 0		
+		
+	cv.create_line( x, y, xEnd, yEnd, width=DEF_WIDTH)	
+	
+	return xEnd, yEnd
 
 master = Tk()
 
@@ -9,68 +90,35 @@ cv = Canvas(master, width=500, height=500)
 cv.pack()
 
 i = 0
-state = 'L'
-xl = {'rtMin':5000 , 'rtMax':10000, 'dMin':5, 'dMax':25, 'thetaMin':0, 'thetaMax':360, 'rMin':5, 'rMax':25, 'phiMin':0, 'phiMax':360} 
-delta = (0,0)
-direction = (0,0)
+
+xl = {'rtMin':5000 , 'rtMax':10000, 'dMin':5, 'dMax':25, 'thetaMin':-70, 'thetaMax':70, 'rMin':5, 'rMax':25, 'phiMin':-45, 'phiMax':45} 
 
 print "Initializing starting position..."
-x2 = random.randint(0,500)
-y2 = random.randint(0,500)
+
+max_iter = random.randint(xl['rtMin'],xl['rtMax'])
+position = 250, 250
+vector = 0
+state = 'L'
 
 print "Generating Random line art..."
-max_iter = random.randint(xl['rtMin'],xl['rtMax'])
-while i < max_iter:
-	x1 = x2
-	y1 = y2
 
+while i < max_iter:
 	state = nextState(state)
 	if state == 'L':
-		delta = init_line(xl['dMin'],xl['dMax'])
-		x2 += delta[0]*direction[0]
-		y2 += delta[1]*direction[1]
-		cv.draw_line(x1,y1,x2,y2, width = defWidth)
+		d = random.randint(xl['dMin'],xl['dMax'])
+		position = drawLine(position[0], position[1], vector, d, cv)
 	elif state == 'A':
-		delta = init_arc(xl['thetaMin'],xl['thetaMax'],xl['rMin'],xl['rMax'])
-		x2 += delta[0]*direction[0]
-		y2 += delta[1]*direction[1]
-		cv.draw_arc(x1,y1,x2,y2, width = defWidth, style = ARC)
+		theta = random.randint(xl['thetaMin'],xl['thetaMax'])
+		r = random.randint(xl['rMin'],xl['rMax'])
+		position = drawArc( position[0], position[1], vector, theta, r, cv)   
 	elif state == 'T':
 		# change direction
-		direction = rotate(xl['phiMin'],xl['phiMax'])
-		break
-
-	theta = random.randint(0,360)
-	r = random.randint(0,random.randint(0,25))
-	x2 += int(r * math.cos(theta))
-	y2 += int(r * math.sin(theta))
- 	print "Coordinates: ", x2, y2
-
-#	test = random.randint(0,3)
-#	if (test == 0):
-#		x2 += random.randint(0,random.randint(0,15))
-#		y2 += random.randint(0,random.randint(0,15))
-#	elif (test == 1):
-#		x2 -= random.randint(0,random.randint(0,15))
-#		y2 -= random.randint(0,random.randint(0,15))
-#	elif (test == 2):
-#		x2 += random.randint(0,random.randint(0,15))
-#		y2 -= random.randint(0,random.randint(0,15))
-#	else:
-#		x2 -= random.randint(0,random.randint(0,15))
-#		y2 += random.randint(0,random.randint(0,15))
-	if x2 > 500:
-		x2 = 500
-	if y2 > 500:
-		y2 = 500
-	if x2 < 0:
-		x2 = 0
-	if y2 < 0:
-		y2 = 0		
-	cv.create_line( x1, y1, x2, y2, width=3)	
+		vector += random.randint(xl['phiMin'],xl['phiMax'])
+		if vector < -360: vector += 360
+		if vector > 360: vector -= 360
 	i = i + 1
-
 	master.update()
+
 print "Saving image file... "
 cv.postscript(file = "output.eps")
 print "Waiting to end..."

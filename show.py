bac873808bff194e8c59f8e8309ee4bd01354c01
Andy Goetz <andy@andygoetz.org>
2012-10-27 19:36:11 -0700

    show.py now renders images if they don't already exist

--- show.py
+++ show.py
@@ -2,7 +2,7 @@
 import cgi
 from dbclient import *
 import pickle
-
+import os
 conn = init_db('bottsydb')
 xcid = 2
 
@@ -12,15 +12,17 @@ print                               # blank line, end of headers
 print '<html>'
 
 form = cgi.FieldStorage()
-if "xcid" not in form:
+if "id" not in form:
     print '<h1>Show Art:</h1>'
 else:
     xcid = int(form['id'].value)
+    
 
 xc = pickle.loads(str(select_xc(conn,xcid)[3]))
 print '<h1>Show Art and xc:</h1>'
 
 score = get_score(conn, xcid)
+os.system("python2 render.py %d" % xcid)
 print "<img src='/bottsy/images/{0}.png'> <br/>Score: {1}<br/>xc: {2}".format(xcid, int(score), xc)
 
 print '<p><a href="/bottsy/bottsy.py">Main Page</a></p>'

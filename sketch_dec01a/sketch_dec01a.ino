b507bde29d87391d99017748e0904761b8f397dc
Andy Goetz <andy@andygoetz.org>
2012-12-02 00:48:48 -0800

    Added tons of comments on code.

--- sketch_dec01a/sketch_dec01a.ino
+++ sketch_dec01a/sketch_dec01a.ino
@@ -48,6 +48,8 @@ const int DIAG_PIN = 13;
 const int LIGHT_MIN = 300;
 const int LIGHT_MAX = 330;
 const int DELAY_QUANTUM = 10;
+
+// helper function to initialize the motor control lines
 void init_motors() 
 {
 	pinMode(LD1, OUTPUT);
@@ -56,6 +58,10 @@ void init_motors()
 	pinMode(RD2, OUTPUT);
 }
 
+// Set a motor's speed and direction.
+// id is LEFT or RIGHT,
+// dir is FWD or REV,
+// power is number from 0 to 255. 
 void set_motor(int id, int dir, char power)
 {
 	int d1, d2;
@@ -84,7 +90,7 @@ void set_motor(int id, int dir, char power)
 
 }
 
-
+// initialization function
 void setup() 
 {
    
@@ -92,16 +98,20 @@ void setup()
      Serial.begin(9600);
      randomSeed(analogRead(0));
 
-     
+     // in order to give the user time to put the robot on the ground,
+     // delay by two seconds before starting code.
      pinMode(DIAG_PIN, OUTPUT);
      digitalWrite(DIAG_PIN,1);
      delay(2000);
      digitalWrite(DIAG_PIN,0);
-     digitalWrite(LED_PIN,0);
-     pinMode(LED_PIN, OUTPUT);
+
+     pinMode(LED_PIN, OUTPUT); 	// enable line-detection led
+     digitalWrite(LED_PIN,0);	// turn on line-detection led
+
      pinMode(BUMPER_PIN, INPUT);           // set pin to input
      digitalWrite(BUMPER_PIN, HIGH);       // turn on pullup resistors
 
+     // set the chromosome state for when it is considered 'light'
      xc.light = {
 	     .islight = TRUE,
 	     .state = LINE,
@@ -117,7 +127,7 @@ void setup()
 
 	     .arc_to_line = 500,
      };
-
+     // set the chromosome state for when it is considered 'dark'
      xc.dark = {
 	     .islight = FALSE,
              .state = LINE,
@@ -135,6 +145,10 @@ void setup()
 
 }
 
+// This function delays the execution of the main program by the
+// amount dictated by len. len is measured in units of DELAY_QUANTUM
+// milliseconds. If one of the bumper switches is pressed, the robot
+// will back up for one second. 
 void smart_delay(int len)
 {
 	for(int i = 0; i < len - 1; i++) {
@@ -151,11 +165,13 @@ void smart_delay(int len)
 }
 
 
-
+// Update the non-deterministic FSMs that determine whether to draw
+// lines or turns. curxc is a pointer to the sub-chromosome that is
+// being updated.
 void update_xc(struct sub_xc * curxc)
 {
 	// checkif if we should switch from line to arc
-	if(state == LINE) {
+	if(curxc->state == LINE) {
 		if(random(1000) < curxc->line_to_arc) {
 			state = ARC;
 		} 
@@ -165,6 +181,11 @@ void update_xc(struct sub_xc * curxc)
 		} 
 	}
 }
+
+// Determine the power to send to each motor, depending on the value
+// of the photo resistor. If the photoresistor detects a dark patch,
+// the dark chromosome will dominate the output. If it detects a light
+// patch, the light chromosome will dominate.
 void fuzzy_mixer(struct chromosome * c_xc, int lightval)
 {
 	int p_range = LIGHT_MAX - LIGHT_MIN;
@@ -213,13 +234,15 @@ void fuzzy_mixer(struct chromosome * c_xc, int lightval)
 	
 }
 
+
+// main iterative loop. 
 void loop () 
 {
-	
-
+	// Read the photoresistor, and send the value to the mixer.
 	int photo = analogRead(PHOTO_PIN);
 	fuzzy_mixer(&xc, photo);
 
+	// update the NFSMs. 
 	update_xc(&xc.light);
 	update_xc(&xc.dark);
 }
